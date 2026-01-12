052db914a9ab4dfc61ad609dae10e2ebfa4dfffc
Yunus Emre EKE <yemreeke1@gmail.com>
2026-01-12 14:26:25 +0300

    feat: build problems fixed

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["yemreeke.dev", 'localhost', '127.0.0.1'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'yemreeke.dev' },
+      { protocol: 'http', hostname: 'localhost' },
+      { protocol: 'http', hostname: '127.0.0.1' },
+    ],
+    unoptimized: true
   },
   trailingSlash: true,
   output: 'export',
-  distDir: 'build',
+  distDir: 'out',
 };
 
 export default nextConfig;
